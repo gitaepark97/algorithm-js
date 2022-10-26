94dc315672400bf1c09ff5a7901b5cba41afc839
gitae park <gitaepark@gitaes-MacBook-Pro.local>
2022-10-26 17:20:12 +0900

    FEAT: queue

---
+++ src/data_structures/queue/Queue.js
@@ -0,0 +1,51 @@
+const { LinkedList } = require('../linked_list/LinkedList');
+
+class Queue {
+  constructor() {
+    this.linkedList = new LinkedList();
+  }
+
+  /**
+   * @returns {boolean}
+   */
+  isEmpty() {
+    return !this.linkedList.head;
+  }
+
+  /**
+   * @returns {any}
+   */
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    return this.linkedList.head.value;
+  }
+
+  /**
+   * @param {any} value
+   */
+  enqueue(value) {
+    this.linkedList.append(value);
+  }
+
+  /**
+   * @returns {any}
+   */
+  dequeue() {
+    const removedHead = this.linkedList.deleteHead();
+
+    return removedHead ? removedHead.value : null;
+  }
+
+  /**
+   * @param {function(any): string} [callback]
+   * @returns {string}
+   */
+  toString(callback) {
+    return this.linkedList.toString(callback);
+  }
+}
+
+module.exports = { Queue };
